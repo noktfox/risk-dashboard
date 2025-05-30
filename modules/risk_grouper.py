fdae9fc37a071e1e3451fb775459d73def712a73
Julia Janusova <julia.janusova@gmail.com>
2025-05-30 20:33:38 +1000

    style(risk_grouper): add comments, return types and variable types

--- modules/risk_grouper.py
+++ modules/risk_grouper.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+
 from config import DEFAULT_N_PEERS
 
 class RiskGrouper:
@@ -10,24 +11,44 @@ class RiskGrouper:
     def __init__(self, n_peers=DEFAULT_N_PEERS):
         self.n_peers = n_peers
 
-    def group(self,
-                  ticker: str,
-                  feature_matrix: pd.DataFrame,
-                  cluster_labels: pd.Series) -> list[str]:
+
+    def group(
+            self,
+            ticker: str,
+            feature_matrix: pd.DataFrame,
+            cluster_labels: pd.Series
+    ) -> list[str]:
         """
-        Given a ticker, gets the four closest tickers in its risk cluster.
+        Gets 'n' closest peer tickers in the same risk cluster.
+
+        :arg:
+            ticker (str): Ticker to get the closest peer tickers for.
+            feature_matrix (pd.DataFrame): Matrix of all sector tickers risk features with tickers as indices.
+            cluster_labels (pd.Series): Series of the closest cluster center of each ticker in a sector,
+                                        with tickers as indices.
+
+        :return:
+            list[str]: List of the closest peer tickers in the same risk cluster.
         """
 
-        label = cluster_labels[ticker]
-        neighbor_tickers = cluster_labels[cluster_labels == label].index.drop(ticker)
-        neighbor_tickers = neighbor_tickers.tolist()
-        if not neighbor_tickers:
+        # Get the cluster label of the given ticker and all other tickers in that cluster
+        ticker_cluster: int = cluster_labels[ticker]
+        peer_tickers = cluster_labels[cluster_labels == ticker_cluster].index.drop(ticker)
+        peer_tickers = peer_tickers.tolist()
+
+        # Return an empty list if no other peers in the same cluster
+        if not peer_tickers:
             return []
 
+        # Get the risk vector of the given ticker and peer tickers in the sector space
         ticker_vector = feature_matrix.loc[ticker].values
-        vectors = feature_matrix.loc[neighbor_tickers].values
+        vectors = feature_matrix.loc[peer_tickers].values
 
+        # Calculate the distance (Euclidean norm) between the ticker and all peer tickers
         distances = np.linalg.norm(vectors - ticker_vector, axis=1)
+        # Order peers based from closest to longest distance
         order = np.argsort(distances)
-        sorted_neighbors = [neighbor_tickers[i] for i in order]
-        return sorted_neighbors[:self.n_peers]
+        sorted_peers = [peer_tickers[i] for i in order]
+
+        # Return 'n' closest peer tickers
+        return sorted_peers[:self.n_peers]
